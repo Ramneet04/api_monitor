427c6a711a1c51805d71dabfc58cbebb0449229c
Ramneet Singh <ramneetsinghrtd@gmail.com>
2026-04-29 17:58:57 +0530

    created models

--- server/src/shared/models/User.js
+++ server/src/shared/models/User.js
@@ -41,15 +41,70 @@ const userSchema = new mongoose.Schema({
                 };
                 return true
             },
-            message: function (props) {
-                if (props.value && !props.value.startsWith('$2a$')) {
+            message: function (props) { // is called when validate is failed
+                if (props.value && !props.value.startsWith('$2a$')) { //props.value is the password being validated
                     const validation = SecurityUtils.validatePassword(props.value)
                     return validation.errors.join(". ");
                 };
                 return "Password validation failed"
             }
         },
+    },
+    role: {
+        type: String,
+        enum: ['super_admin', 'client_admin', 'client_viewer'],
+        default: 'client_viewer'
+    },
+    clientId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Client',
+        required: function () {
+            return this.role !== 'super_admin';
+        }
+    },
+    isActive: {
+        type: Boolean,
+        default: true
+    },
+    Permissions: {
+        canCreateApiKeys: {
+            type: Boolean,
+            default: false,
+        },
+        canManageUsers: {
+            type: Boolean,
+            default: false,
+        },
+        canViewAnalytics: {
+            type: Boolean,
+            default: true,
+        },
+        canExportData: {
+            type: Boolean,
+            default: false,
+        }
+    }
+}, {
+    timestamps: true,
+    collection: 'users'
+});
 
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        return next();
     }
 
-})
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+userSchema.index({ clientId: 1, isActive: 1 });
+userSchema.index({ role: 1 });
+
+const User = mongoose.model('User', userSchema);
+export default User;
